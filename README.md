38553d58a48a9624e343acd7877ad273be46ace5
Sean Hodges <sean.hodges@transferwise.com>
2019-02-27 18:54:28 +0000

    Add the lw-command.py tool

--- README.md
+++ README.md
@@ -4,13 +4,21 @@
 
 A basic Python library for Lightwave "v2" LinkPlus API.
 
-## Usage
+##  Tool usage
 
-This project comes with some simple command-line tools that demonstrate the usage of the library.
+This project comes with some simple command-line tools that demonstrate how to use the library. Use `--help` to get usage help.
+
+* lw-probe.py - Retrieve device and feature information (including feature ID's)
+* lw-command.py - Send commands to device capabilities (features)
+* lw-event.py - Listen to events from v2 devices
+
+##  Library usage
 
 You must authenticate before you can send commands, obtain a bearer token and refresh token from here: https://my.lightwaverf.com/settings/general/api
 
-Then use the tokens to generate a full access token for use with the client. As below:
+_Note:_ Likely you'll need to refresh your new token before first use.
+
+Then use the tokens to generate a full access token for use with the client. Simple example below:
 
 ```python
 #!/usr/bin/env python
@@ -19,7 +27,7 @@ from pylightwave.auth import LWAuth
 from pylightwave.client import LWClient
 
 lw_auth = LWAuth()
-tokens = lw_auth.authenticate('<bearer_token>', '<refresh_token>')
+tokens = lw_auth.refresh('<bearer token>', '<refresh token>')
 lw_client = LWClient(tokens)
-print lw_client.get_feature_details('<feature_id>')
+print lw_client.get_feature_value('<feature id>')
 
---
+++ lw-command.py
@@ -0,0 +1,60 @@
+"""LW command tool - send commands to devices
+
+Usage:
+  lw-command.py authenticate <bearer> <refresh>
+  lw-command.py set <feature id> <value>
+  lw-command.py get <feature id> <value>
+  lw-command.py (-h | --help)
+  lw-command.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+
+import os
+import yaml
+from docopt import docopt
+
+from pylightwave.auth import LWAuth
+from pylightwave.client import LWClient
+
+
+class AuthStore:
+
+    def store_token(self, auth):
+        print auth['access_token']
+        with open('.auth.yaml', 'w') as file_handle:
+            yaml.safe_dump(auth, file_handle, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+
+
+    def get_token(self):
+        if os.path.isfile('.auth.yaml'):
+            with open('.auth.yaml', 'r') as stream:
+                return yaml.load(stream)
+        else:
+            raise 'Please authenticate first :)'
+
+
+def main():
+    arguments = docopt(__doc__, version='1.0')
+    auth_store = AuthStore()
+
+    if arguments['authenticate'] and arguments['<bearer>'] and arguments['<refresh>']:
+        lw_auth = LWAuth()
+        auth = lw_auth.refresh(arguments['<bearer>'], arguments['<refresh>'])
+        auth_store.store_token(auth)
+    elif arguments['set'] and arguments['<feature id>'] and arguments['<value>']:
+        client = LWClient(auth_store.get_token())
+        client.set_feature_value(arguments['<feature id>'], arguments['<value>']
+    elif arguments['get'] and arguments['<feature id>']:
+        client = LWClient(auth_store.get_token())
+        print client.get_feature_value(arguments['<feature id>']
+    else:
+        print 'Command not supported at this time'
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
